553ff2a91d115df14427f448dcb2fdf71e5afc44
grizzful <150555435+grizzful@users.noreply.github.com>
2023-11-11 15:59:09 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import random
+# Tic Tac toe
+
+def get_user_choice():
+    choice = input("Enter your choice (Rock/Paper/Scissors): ")
+    while choice.lower() not in ['rock', 'paper', 'scissors']:
+        choice = input("Invaild option. Please enter again (Rock/Paper/Scissors): ")
+    return choice
+
+def get_bot_choice():
+    choices = ['rock', 'paper', 'scissors']
+    return random.choice(choices)
+
+def decide_winner():
+    if user_choice == bot_choice:
+        print("It is a tie!")
+    elif user_choice == "rock":
+        if bot_choice == "scissors":
+            win = "User"
+        else:
+            win = "bot"
+
+    elif user_choice == "paper":
+        if bot_choice == "rock":
+            win = "User"
+        else:
+            win = "bot"
+
+    elif user_choice == "scissors":
+        if bot_choice == "paper":
+            win = "User"
+        else:
+            win = "bot"
+    return win
+
+# Start of main program
+user_choice = get_user_choice()
+bot_choice = get_bot_choice()
+winner = decide_winner()
+print("You picked:", user_choice)
+print("The bot picked:", bot_choice)
+print("The winner is:", winner)
